6bf8f2f148059115bbb728e81bac9289c8bfa651
ginakui <developerkui@outlook.com>
2019-10-02 10:09:40 -0400

    add ProjectCard propType

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Card, CardImg, CardText, CardBody, CardLink,
   CardTitle
@@ -11,16 +12,30 @@ const ProjectCard = ({ project }) => {
   return (
     <div className='ProjectCard'>
       <Card>
-        <CardImg top src={img} alt='project screenshot' width='200px' height='150px'/>
+        <CardImg top src={img} alt='app screenshot' width='200px' height='150px'/>
         <CardBody>
           <CardTitle>{name}</CardTitle>
           <CardText>{description}</CardText>
-          <CardLink href="#">github</CardLink>
-          <CardLink href="#">live</CardLink>
+          { project.src_url && <CardLink href={project.src_url}><i className="fab fa-github" title="github source code"></i></CardLink> }
+          { project.deploy_url && <CardLink href="project.deploy_url">deployment</CardLink> }
         </CardBody>
       </Card>
     </div>
   )
-}
+};
 
+ProjectCard.defaultProps = {
+  project: {
+    name: 'project',
+    description: 'brief description',
+    img: 'image url'
+  }
+};
+ProjectCard.propTypes = {
+  project: PropTypes.shape({
+    name: PropTypes.string,
+    description: PropTypes.string,
+    img: PropTypes.string,
+  }).isRequired,
+}
 export default ProjectCard;
