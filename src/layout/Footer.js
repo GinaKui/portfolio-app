bf13d6d410caeec7564a4176d26bce9e8c0a341e
ginakui <developerkui@outlook.com>
2020-12-23 11:28:26 -0500

    add <a> to footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="Footer">
-      <p>By Gina Kui, developer@outlook.com</p>
+      <p>By <a href="https://github.com/GinaKui" target="_blank" title="github">Gina Kui</a>, <a href="@mailto:developerkui@outlook.com">developer@outlook.com</a></p>
     </footer>
   )
 }
